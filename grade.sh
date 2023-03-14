d19b2b413252058aabc982212d12a292decb82f9
bretdubois <bdubois@ucsd.edu>
2023-03-13 19:10:18 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,10 +1,19 @@
-CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
+# Set CPATH using OS type
+if [[ $(uname) == "Darwin" ]]; then
+    CPATH='.:..lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
+else
+    CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
+fi
+
+# Save JUnit output to txt file
 OUTPUT_FILE='junit_output.txt'
 
+# Remove previous student-submission and clone new submission
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
+# Copy file and check if successful
 cp TestListExamples.java student-submission
 
 cd student-submission
@@ -13,12 +22,12 @@ then
     echo "ListExamples.java file present"
 else
     set -e
-    echo "Could not find ListExamples.java"
+    echo "Could not find ListExamples.java, check file name"
     exit 1
 fi
 
+# Compile java files and check for compile errors
 javac -cp $CPATH *.java
-
 if [ $? -eq 0 ]
 then
     echo "Java files compiled successfully"
@@ -28,16 +37,36 @@ else
     exit 1
 fi
 
+# Run JUnit tests and output results
+# Calculate letter grade from output file
 java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > $OUTPUT_FILE
+if grep -q "FAILURES" $OUTPUT_FILE; then
+    cat $OUTPUT_FILE
+    TESTS=$(grep 'Tests run' $OUTPUT_FILE)
+    IFS=", " read -a TESTLINE <<< $TESTS
 
-TEST_OK=$(grep 'OK' $OUTPUT_FILE)
+    TOTAL="${TESTLINE[2]}"
+    FAILS="${TESTLINE[4]}"
+    PASSES=$((TOTAL - FAILS))
 
-if [ -n "$TEST_OK" ]; then
-    GRADE=100
-    echo "GRADE: $GRADE"
+    if [[ $PASSES -eq $TOTAL ]]; then
+        LETTER="A+"
+    elif [[ $PASSES -gt $((TOTAL / 2)) ]]; then
+        LETTER="A"
+    elif [[ $PASSES -gt $((TOTAL / 5)) ]]; then
+        LETTER="B"
+    elif [[ $PASSES -gt $((TOTAL / 10)) ]]; then
+        LETTER="C"
+    elif [[ $PASSES -gt 0 ]]; then
+        LETTER="D"
+    else
+        LETTER="F"
+    fi
+
+    GRADE=$(( PASSES * 100 / TOTAL ))
+    echo "$GRADE" "$LETTER"
     exit 0
 else
-    GRADE=0
-    echo "GRADE: $GRADE"
+    echo "All passing - A+"
     exit 0
 fi
